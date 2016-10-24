730b6a9e8abb26c3b5168d110b948978c931c2ac
Cedric Nicoloso <cedricnicoloso@qantas.com.au>
2016-10-24 16:33:35 +1100

    Add comments to server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
+// Require dependencies
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
+// Require our schema
 const schema = require('./schema');
 
+// Create our Express server
 const app = express();
 
+// Define the /graphql route
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true,
 }));
+/*
+ * ‘graphiql: true’ will activate an online explorer with API documentation.
+ * This feature is highly recommended in development, and can optionally be deactivated in production.
+ */
 
+// Finally start the server
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
